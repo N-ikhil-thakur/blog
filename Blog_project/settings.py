86a1eaf712f09a109f47e83f7efaa084b48dba78
unknown <nikhilthakur281@gmail.com>
2020-05-08 20:35:23 +0545

    setup

--- Blog_project/settings.py
+++ Blog_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '76xf!=^g+x^_%=*()2he#z8q$2_ofudt&7j(a!))w8xvj4vuxt'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
